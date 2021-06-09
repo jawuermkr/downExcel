38116b9d75525ca9142a090eda07b720db8b039f
Nelson Jawuer Morales Santa <guitarro92@hotmail.com>
2021-06-09 11:48:52 -0500

    Indicaciones

--- excel.php
+++ excel.php
@@ -5,37 +5,37 @@
 
 ?>
 
-<div class="col-md-12">
-    <table class="table table-striped table-bordered">
-    <tr>
-    <th>ID Agente</th>
-    <th>Nombre Agente</th>
-    <th>Tipo ID Cliente</th>
-    <th>ID Cliente</th>
-    <th>Nombre Cliente</th>
-    <th>Celular</th>
-    <th>Tipo Venta</th>
-    <th>Referencia</th>
-    <th>Valor</th>
-    <th>Campana</th>
-    <th>Fecha</th>
-    </tr>
+<!--- Aquí va la consulta a la base de datos  que se mostrará en la siguiente tabla. --->
+
+<div>
+    <table>
+        <tr>
+        <th>ID</th>
+        <th>Nombre</th>
+        <th>Tipo ID Cliente</th>
+        <th>ID Cliente</th>
+        <th>Nombre Cliente</th>
+        <th>Celular</th>
+        <th>Tipo Venta</th>
+        <th>Referencia</th>
+        <th>Valor</th>
+        <th>Campana</th>
+        <th>Fecha</th>
+        </tr>
 
     <tbody>
-    <?php foreach($libros as $libro) { ?>
-    <tr>
-    <td>Datos OK ok</td>
-    <td>Datos OK ok</td>
-    <td>Datos OK ok</td>
-    <td>Datos OK ok</td>
-    <td>Datos OK ok</td>
-    <td>Datos OK ok</td>
-    <td>Datos OK ok</td>
-    <td>Datos OK ok</td>
-    <td>Datos OK ok</td>
-    <td>Datos OK ok</td>
-    <td>Datos OK ok</td>
-    </tr>
-    <?php } ?>
+        <tr>
+        <td>Datos OK ok</td>
+        <td>Datos OK ok</td>
+        <td>Datos OK ok</td>
+        <td>Datos OK ok</td>
+        <td>Datos OK ok</td>
+        <td>Datos OK ok</td>
+        <td>Datos OK ok</td>
+        <td>Datos OK ok</td>
+        <td>Datos OK ok</td>
+        <td>Datos OK ok</td>
+        <td>Datos OK ok</td>
+        </tr>
     </tbody>
     </table>
